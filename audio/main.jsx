1fcaa61bdb10610a79d3404422acfdc6c5fa0cfa
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2024-01-13 12:48:33 -0600

    added audio file

--- audio/main.jsx
+++ audio/main.jsx
@@ -1,1 +1,12 @@
 //javascript page for audio project
+
+var context;
+window.addEventListener('load', init, false);
+function init() {
+    try {
+    context = new AudioContext();
+    }
+    catch(e) {
+    alert('Web Audio API is not supported in this browser');
+    }
+}
