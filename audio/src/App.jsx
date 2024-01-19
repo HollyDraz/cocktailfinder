de7188cfc2e7ea28b233d64737079552e84ff2a8
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2024-01-19 11:52:32 -0600

    crete app.jsx

---
+++ audio/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+function App() {
+console.log('in the fucntion app');
+
+
+}
+
+
+export default App;
--- audio/src/main.jsx
+++ audio/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import App from '.App.jsx';
 import { createRoot } from 'react-dom/client';
 
 
+console.log('hieee');
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
@@ -10,7 +12,8 @@ document.body.innerHTML = '<div id="app"></div>';
 const root = createRoot(document.getElementById('app'));
 root.render(<h1>Hello, world</h1>);
 
-console.log('hieee');
 
+
+//import the audio from the file 
 // let beat = new Audio('audio.mp3');
 
