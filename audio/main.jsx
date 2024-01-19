fa21b1b32c438cca5b408a96539f882498196757
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2024-01-19 11:20:10 -0600

    added react

--- audio/main.jsx
+++ audio/main.jsx
@@ -1,1 +1,14 @@
-let beat = new Audio('audio.mp3');
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+
+console.log('hieee');
+
+// let beat = new Audio('audio.mp3');
+
